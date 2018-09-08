ffc1dcb0f9264d8da682c822755aa171f72dc58e
arshivbastian <37534259+arshivbastian@users.noreply.github.com>
2018-09-08 19:57:07 +0430

    1st thing 1st
    root + base page

---
+++ root/js/site.js
@@ -0,0 +1,29 @@
+﻿// Write your JavaScript code.
+var notification = $("#notification").kendoNotification({
+    position: {
+        pinned: true,
+        top: null,
+        right: null,
+        left: 30,
+        button:30
+    },
+    autoHideAfter:5000,
+  
+    templates: [{
+        type: "info",
+        template: $("#emailTemplate").html()
+    }, {
+        type: "error",
+        template: $("#errorTemplate").html()
+    }, {
+        type: "success",
+        template: $("#successTemplate").html()
+    }, {
+        type: "warning",
+        template: $("#warningTemplate").html()
+    }
+               ]
+
+}).data("kendoNotification");
+
+
